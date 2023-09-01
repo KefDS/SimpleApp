f1045d5ad15ecbc42a9de400f6bf65c5d5498cd4
Kevin Delgado <kdelgado@microsoft.com>
2023-09-01 13:43:49 -0600

    Add project files.

---
+++ SimpleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, World!");
+Thread.Sleep(1000);
+Console.WriteLine("Tell me somthing");
+string input = Console.ReadLine();
+
+Console.WriteLine("Processing");
+Thread.Sleep(2000);
+
+Console.WriteLine($"You said {(input == String.Empty ? "nothing" : input)}");
+
